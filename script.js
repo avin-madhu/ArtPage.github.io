aee16babaa4b16709e380c91aa8af3e7220bb2fb
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2022-10-09 18:31:55 +0700

    inital setup

---
+++ script.js
@@ -0,0 +1,12 @@
+/*const button = document.getElementById('delegar');
+button.addEventListener('click', (event) => {
+    if (button.innerHTML === "Dark Mode") {
+        document.documentElement.style.setProperty('--text', '#eee');
+        document.documentElement.style.setProperty('--bg', '#1e2939');
+        button.innerHTML = "Light Mode";
+    } else {
+        document.documentElement.style.setProperty('--text', '#1e2939');
+        document.documentElement.style.setProperty('--bg', '#F1F9fc');
+        button.innerHTML = "Dark Mode";
+    }
+});*/   // -- Dark mode toggle code
